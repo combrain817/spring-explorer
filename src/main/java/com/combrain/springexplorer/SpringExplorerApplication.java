f2c875c2109622805b103e4f5c93748cdf6ee670
combrain <skrtn3410@gmail.com>
2023-11-09 23:11:53 +0900

    project setup

---
+++ src/main/java/com/combrain/springexplorer/SpringExplorerApplication.java
@@ -0,0 +1,13 @@
+package com.combrain.springexplorer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringExplorerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringExplorerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/combrain/springexplorer/SpringExplorerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.combrain.springexplorer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringExplorerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
